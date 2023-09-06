8b3fa96eebc3bfd20c17815b7c01f5a72b4cf142
Mcgatsby <ayanda99zulu@gmail.com>
2023-09-06 17:57:47 +0200

    header and iamge added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
 import './App.css';
+import logo from './assets/images/logo.svg'
+import menu from './assets/images/icon-menu.svg'
+import web3Mobile from './assets/images/image-web-3-mobile.jpg'
+
 
 function App() {
   return (
-    <div className="App">
-      HELLO
+    <div className="app-container">
+      <header>
+        <img src={logo} alt="log" width={30}/>
+        <img className='menu' src={menu} alt="menu icon" width={30}/>
+      </header>
+      <div className="content grid-container">
+        <div className="grid-item web3-image">
+          <img className='web3-mobile' src={web3Mobile} alt="web 3 image"/>
+        </div>
+      </div>
     </div>
   );
 }
